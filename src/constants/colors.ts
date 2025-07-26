2cd905ac737d829df623d34ee289e3d3647f6e96
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-07-26 12:29:44 -0700

    Defining constant colors object

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+// Standard colors used throughout the WaniKani app
+
+export const COLORS: { [key: string]: string} = {
+    VOCAB_PURPLE: "#AA00FF",
+    KANJI_PINK: "#FF00AA",
+    RADICAL_BLUE: "#00AAFF"
+};
