c50d9cd8e98eb0b0c09f5a7c472950142f6f0057
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-09-29 11:32:36 -0700

    Refactor ReviewsButton to enhance layout, integrate review count display, and improve loading state handling

--- src/components/ReviewsButton.tsx
+++ src/components/ReviewsButton.tsx
@@ -1,31 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { View, Pressable } from 'react-native';
+import { View, Text, Image, Button, Pressable, LayoutChangeEvent} from 'react-native';
 
 import { ReviewsAPI } from '../api/reviews';
 import { HomeStyles } from '../styles/globals';
+import { HomeButtonStyles } from '../styles/homebutton.styles';
 import { Colors } from '../constants/colors';
 
-interface ReviewsButtonProps {
+interface LessonsButtonProps {
     label: string
 }
 
-const ReviewsButton: React.FC<ReviewsButtonProps> = ({ label }) => {
+const ReviewsButton: React.FC<LessonsButtonProps> = ({ label }) => {
+    const [reviewCount, setReviewCount] = useState(0);
+    const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
     const [authorized, setAuthorized] = useState<boolean | null>(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchUser = async () => {
+        const fetchReviews = async () => {
             try {
                 const result = await ReviewsAPI.getAllReviews();
-                if (result) setAuthorized(true);
+                if (result) {
+                    setReviewCount(result.total_count);
+                    setAuthorized(true);
+                }
             } catch (e) {
                 console.error(e);
             } finally {
                 setLoading(false);
             }
         };
-        fetchUser();
+        fetchReviews();
     }, []);
+
+    const onLayout = (event: LayoutChangeEvent) => {
+        const { width, height } = event.nativeEvent.layout;
+        setContainerSize({ width, height });
+    };
+    const maxWidth = containerSize.width * 0.5; // 50% of container width
+    const maxHeight = containerSize.height * 0.75; // 75% of container height
 
     if (loading) {
         return (
@@ -38,9 +51,31 @@ const ReviewsButton: React.FC<ReviewsButtonProps> = ({ label }) => {
     <Pressable
       style={[
         HomeStyles.review_box,
-        authorized ? { backgroundColor: Colors.KANJI_PINK } : { backgroundColor: 'gray' },
+        authorized ? { backgroundColor: Colors.RADICAL_BLUE } : { backgroundColor: 'gray' },
       ]}
+      onLayout={onLayout}
     >
+      <View style={HomeButtonStyles.count}>
+        <Text>Reviews {reviewCount}</Text>
+        <Text>Review these items to level them up!</Text>
+      </View>
+
+      <View style={HomeButtonStyles.icon}>
+        {containerSize.width > 0 && (
+          <Image
+            source={require('../assets/images/buttons/review_kappa.png')}
+            style={{
+              width: maxWidth,
+              height: maxHeight,
+              resizeMode: 'contain',
+            }}
+          />
+        )}
+      </View>
+
+      <View style={HomeButtonStyles.button}>
+        <Button title='Start Reviews'/>
+      </View>
     </Pressable>
   );
 }
