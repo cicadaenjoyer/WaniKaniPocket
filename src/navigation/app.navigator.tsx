44e946e98f659a2309a8a4871a9ac6ff3e2ee695
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-11-04 14:26:14 -0800

    Adding Reviews screen to navigator

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import * as SecureStore from "expo-secure-store";
 
 // Screens
-import LoginScreen from '../screens/LoginScreen';
-import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from "../screens/LoginScreen";
+import HomeScreen from "../screens/HomeScreen";
+import ReviewScreen from "../screens/ReviewScreen";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const AppNavigator = () => {
-    const hasToken = SecureStore.getItem('WK_TOKEN');
-    const initialRoute = hasToken ? 'Home' : 'Login';
+    const hasToken = SecureStore.getItem("WK_TOKEN");
+    const initialRoute = hasToken ? "Home" : "Login";
 
     return (
-    <NavigationContainer>
-        <Navigator initialRouteName={initialRoute} screenOptions={navigatorOptions}>
-            <Screen name='Home' component={HomeScreen}></Screen>
-            <Screen name='Login' component={LoginScreen}></Screen>
-        </Navigator>
-    </NavigationContainer>
-    )
-}
+        <NavigationContainer>
+            <Navigator
+                initialRouteName={initialRoute}
+                screenOptions={navigatorOptions}
+            >
+                <Screen name="Home" component={HomeScreen}></Screen>
+                <Screen name="Login" component={LoginScreen}></Screen>
+                <Screen name="Review" component={ReviewScreen}></Screen>
+            </Navigator>
+        </NavigationContainer>
+    );
+};
 
 const navigatorOptions = {
-    headerShown: false
-}
+    headerShown: false,
+};
 
-export default AppNavigator
+export default AppNavigator;
