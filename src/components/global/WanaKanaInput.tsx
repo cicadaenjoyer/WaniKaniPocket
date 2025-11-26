b62dcb4806f709ff17515e2a52055df69ca7756d
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-11-25 17:09:31 -0800

    WanaKanaInput: Checking for correct answers on submission

--- src/components/global/WanaKanaInput.tsx
+++ src/components/global/WanaKanaInput.tsx
@@ -1,30 +1,48 @@
 import React, { useState, forwardRef, useImperativeHandle } from "react";
-import { TextInput, TextProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { isHiragana, toHiragana } from "wanakana";
-
-import { ReviewStyles } from "../../styles/globals";
 
 export interface WanaKanaInputRef {
     clear: () => void;
+    isCorrect: () => boolean;
     isValid: () => boolean;
     getText: () => string;
 }
 
-const WanaKanaInput = forwardRef<WanaKanaInputRef, TextProps>(
+interface WKI_Props extends TextInputProps {
+    q_type: "reading" | "meaning";
+    answers: { reading: string; meaning: string }[];
+}
+
+const WanaKanaInput = forwardRef<WanaKanaInputRef, WKI_Props>(
     ({ style, ...props }, ref) => {
         const [text, setText] = useState("");
 
         const handleChangeText = (input: string) => {
-            const hiragana = toHiragana(input);
-            setText(hiragana);
+            if (props.q_type === "reading") {
+                const hiragana = toHiragana(input);
+                setText(hiragana);
+            } else {
+                setText(input);
+            }
         };
 
         useImperativeHandle(ref, () => ({
             clear: () => {
                 setText("");
             },
+            isCorrect: () => {
+                const A_TYPE = props.q_type;
+
+                const answer = props.answers.find(
+                    (a) => a[A_TYPE]?.toLowerCase() === text.toLowerCase()
+                );
+                return !!answer;
+            },
             isValid: () => {
-                return isHiragana(text);
+                return props.q_type === "reading"
+                    ? isHiragana(text)
+                    : text.length > 0;
             },
             getText: () => {
                 return text;
@@ -33,7 +51,7 @@ const WanaKanaInput = forwardRef<WanaKanaInputRef, TextProps>(
 
         return (
             <TextInput
-                style={[ReviewStyles.input, style]}
+                style={style}
                 {...props}
                 value={text}
                 onChangeText={handleChangeText}
