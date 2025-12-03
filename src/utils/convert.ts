7e96746dbf645e17caa80aa71502348271a2607d
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-12-03 11:34:03 -0800

    convert.ts: adding a function for individual subject obj conversions and adding to interface

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -13,50 +13,82 @@ interface Subject {
     data: {
         slug: string;
         characters: string;
+        character_images: Array<{ url: string }>;
         readings: any;
         meanings: any;
+        meaning_mnemonic: string;
+        reading_mnemonic: string;
+        meaning_hint: string;
+        reading_hint: string;
+        pronunciation_audios: Array<{
+            url: string;
+            metadata: {
+                gender: "string";
+                voice_actor_id: number;
+                voice_actor_name: "string";
+                voice_description: "string";
+            };
+        }> | null;
     };
+    srs_stage: number;
+}
+
+function convertSubject(subject_raw: Subject) {
+    const partial_subject = {
+        fill: "",
+        type: subject_raw.object,
+        q_type: "",
+        slug: subject_raw.data.slug,
+        characters: subject_raw.data.characters,
+        character_image: subject_raw.data.character_images?.[0]?.url || "",
+        readings: subject_raw.data.readings,
+        meanings: subject_raw.data.meanings,
+        m_explanation: subject_raw.data.meaning_mnemonic,
+        r_explanation: subject_raw.data.reading_mnemonic,
+        m_hint: subject_raw.data.meaning_hint,
+        r_hint: subject_raw.data.reading_hint,
+        // NOTE: API is returning dupe audios for some reason...
+        pronunciation_audios: subject_raw.data.pronunciation_audios?.filter(
+            (audio, index, self) =>
+                index ===
+                self.findIndex(
+                    (a) =>
+                        a.metadata.voice_actor_id ===
+                        audio.metadata.voice_actor_id
+                )
+        ),
+    };
+
+    const subject_meaning = partial_subject;
+    const subject_reading = partial_subject;
+
+    subject_meaning.q_type = "meaning";
+    subject_reading.q_type = "reading";
+    switch (subject_raw.object) {
+        case "radical":
+            subject_reading.fill = Colors.RADICAL_BLUE;
+            subject_meaning.fill = Colors.RADICAL_BLUE;
+            break;
+        case "kanji":
+            subject_reading.fill = Colors.KANJI_PINK;
+            subject_meaning.fill = Colors.KANJI_PINK;
+            break;
+        case "vocabulary":
+            subject_reading.fill = Colors.VOCAB_PURPLE;
+            subject_meaning.fill = Colors.VOCAB_PURPLE;
+            break;
+    }
+
+    return [subject_meaning, subject_reading];
 }
 
 function convertSubjects(subjects_raw: Subject[]) {
     return subjects_raw.flatMap((subject) => {
-        const [subject_reading, subject_meaning] = [
-            {
-                fill: "",
-                type: subject.object,
-                q_type: "reading",
-                slug: subject.data.slug,
-                characters: subject.data.characters,
-                readings: subject.data.readings,
-            },
-            {
-                fill: "",
-                type: subject.object,
-                q_type: "meaning",
-                slug: subject.data.slug,
-                characters: subject.data.characters,
-                meanings: subject.data.meanings,
-            },
-        ];
-        switch (subject.object) {
-            case "radical":
-                subject_reading.fill = Colors.RADICAL_BLUE;
-                subject_meaning.fill = Colors.RADICAL_BLUE;
-                break;
-            case "kanji":
-                subject_reading.fill = Colors.KANJI_PINK;
-                subject_meaning.fill = Colors.KANJI_PINK;
-                break;
-            case "vocabulary":
-                subject_reading.fill = Colors.VOCAB_PURPLE;
-                subject_meaning.fill = Colors.VOCAB_PURPLE;
-                break;
-        }
-
-        return [subject_meaning, subject_reading];
+        return convertSubject(subject);
     });
 }
 
 export const C_Utils = {
+    convertSubject,
     convertSubjects,
 };
