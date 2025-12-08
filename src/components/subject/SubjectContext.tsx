8e0933f9ac848b6a4e2a7b65304998846ee0ca02
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-12-08 10:39:37 -0800

    New SubjectContext component
    New component for `SubjectDefinition` and used for Vocab subjects.
    Currently only displays Context Sentences

---
+++ src/components/subject/SubjectContext.tsx
@@ -0,0 +1,72 @@
+/**
+ * SubjectContext component displays a list of example context sentences for the
+ * vocab subject
+ *
+ * @param {SubjectSlugProps} props - The props for the SubjectContext component.
+ * @param {string} props.context_sentences - The context sentences in both English and Japanese.
+ *
+ * @returns {JSX.Element}
+ */
+import React from "react";
+import { View, Text } from "react-native";
+
+// Styling
+import { Colors } from "../../constants/colors";
+
+interface SubjectContextProps {
+    context_sentences: Array<{ en: string; ja: string }>;
+}
+
+const SubjectContext: React.FC<SubjectContextProps> = ({
+    context_sentences,
+}) => {
+    return (
+        <View style={{ marginTop: 12 }}>
+            {/* Meaning Header and Divider */}
+            <Text
+                style={{
+                    fontFamily: "NotoSans-Regular",
+                    fontSize: 20, // NOTE: make dynamic later
+                    color: Colors.BASIC_BLACK,
+                }}
+            >
+                Context
+            </Text>
+            <View
+                style={{
+                    height: 1,
+                    width: "100%",
+                    backgroundColor: Colors.OPTIONS_GREY,
+                    marginBottom: 12,
+                }}
+            />
+
+            {/* Subject Context Sentences */}
+            <Text
+                style={{
+                    fontFamily: "NotoSans-Regular",
+                    fontSize: 14, // NOTE: make dynamic later
+                    color: Colors.LESSON_GREY,
+                }}
+            >
+                Context Sentences
+            </Text>
+            {context_sentences.map((c_sent) => {
+                return (
+                    <View
+                        style={{
+                            paddingBottom: 12,
+                        }}
+                    >
+                        <Text>{c_sent.ja}</Text>
+                        <Text style={{ color: Colors.LESSON_GREY }}>
+                            {c_sent.en}
+                        </Text>
+                    </View>
+                );
+            })}
+        </View>
+    );
+};
+
+export default SubjectContext;
