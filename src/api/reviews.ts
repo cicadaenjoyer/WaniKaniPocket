0bd8ec6ef6d88fe2b55e5f43ddf44753bed24ce0
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-12-10 12:58:20 -0800

    Creating API endpoints for PUT/POST commands in assignments and reviews

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -1,35 +1,47 @@
 import * as SecureStore from "expo-secure-store";
 
+// Interfaces
+import { ReviewProps } from "../interfaces/Review";
+
 const WEB_URL = "https://api.wanikani.com/v2";
 
-async function getAllReviews() {
-    const apiToken = SecureStore.getItem('WK_TOKEN');
+async function postReview(review: ReviewProps) {
+    const apiToken = SecureStore.getItem("WK_TOKEN");
 
     if (apiToken) {
         const headers: Headers = new Headers();
-        headers.set('Authorization', `Bearer ${apiToken}`);
-        headers.set('Wanikani-Revision', '20170710');
+        headers.set("Authorization", `Bearer ${apiToken}`);
+        headers.set("Wanikani-Revision", "20170710");
+        headers.set("Content-Type", "application/json");
 
         const response = await fetch(`${WEB_URL}/reviews`, {
-            method: "GET",
-            headers: headers
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify({
+                review: {
+                    created_at: review.created_at,
+                    assignment_id: review.assignment_id,
+                    incorrect_meaning_answers: review.incorrect_meaning_answers,
+                    incorrect_reading_answers: review.incorrect_reading_answers,
+                },
+            }),
         });
 
         if (!response.ok) {
-            switch (response.status){
+            switch (response.status) {
                 case 401:
                     throw new Error("Unauthorized: Invalid API token");
                 case 404:
                     throw new Error("Not Found: Endpoint does not exist");
                 default:
-                    throw new Error(`API Error: ${response.status} ${response.statusText}`);
+                    throw new Error(
+                        `API Error: ${response.status} ${response.statusText}`
+                    );
             }
-        } else {
-            return await response.json();
         }
     }
 }
 
 export const ReviewsAPI = {
-    getAllReviews,
-}
+    postReview,
+};
---
+++ src/interfaces/Review.ts
@@ -0,0 +1,7 @@
+export interface ReviewProps {
+    assignment_id?: number;
+    started_at?: string;
+    created_at?: string;
+    incorrect_meaning_answers?: number;
+    incorrect_reading_answers?: number;
+}
