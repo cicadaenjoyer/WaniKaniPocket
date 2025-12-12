f5fc59be38d218d6e54bf4f842d61c774ae472df
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-12-12 11:09:50 -0800

    Shifting api calls for reviews and assignments to home screen for the purpose of rerendering

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,9 +8,11 @@
 import React, { useState, useEffect } from "react";
 import { View, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useFocusEffect } from "@react-navigation/native";
 
 // API
 import { UserAPI } from "../api/user";
+import { AssignmentsAPI } from "../api/assignments";
 
 // Components
 import Reviews from "../components/home/dashboard/Reviews";
@@ -22,21 +24,64 @@ import { HomeStyles } from "../styles/globals";
 const HomeScreen = () => {
     const [userLevel, setUserLevel] = useState(0);
     const [userPref, setUserPref] = useState({});
+    const [assignments, setAssignments] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const user = await UserAPI.getUserInfo();
-                if (user) {
-                    setUserLevel(user.data.level);
-                    setUserPref(user.data.preferences);
-                }
-            } catch (e) {
-                console.error(e);
+    const [loading, setLoading] = useState(false);
+
+    const fetchUser = async () => {
+        try {
+            const user = await UserAPI.getUserInfo();
+            if (user) {
+                setUserLevel(user.data.level);
+                setUserPref(user.data.preferences);
             }
-        };
-        fetchUser();
-    }, []);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    const fetchAssignments = async () => {
+        try {
+            const assignments = await AssignmentsAPI.getAssignmentsBatch();
+            if (assignments) {
+                setAssignments(assignments.data);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    const fetchReviews = async () => {
+        try {
+            const reviews = await AssignmentsAPI.getAvailableReviews();
+            if (reviews) {
+                setReviews(reviews.data);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    // Refresh the page and its components on back page event
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchUser();
+            fetchAssignments();
+            fetchReviews();
+            setLoading(false);
+        }, [])
+    );
+
+    // Default loading state view
+    if (loading) {
+        return (
+            <View
+                style={[
+                    HomeStyles.review_box,
+                    { justifyContent: "center", alignItems: "center" },
+                ]}
+            ></View>
+        );
+    }
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -52,11 +97,19 @@ const HomeScreen = () => {
 
                     {/* Assignments & Reviews Dashboard */}
                     <View style={HomeStyles.review_container}>
-                        <Assignments
-                            label="Assignments"
-                            userPref={userPref}
-                        ></Assignments>
-                        <Reviews label="Reviews"></Reviews>
+                        {assignments && (
+                            <Assignments
+                                label="Assignments"
+                                assignments={assignments}
+                                userPref={userPref}
+                            ></Assignments>
+                        )}
+                        {reviews && (
+                            <Reviews
+                                label="Reviews"
+                                reviews={reviews}
+                            ></Reviews>
+                        )}
                     </View>
 
                     {/* Current Level & Radical/Kanji Progress */}
